68a12fb923c1c06d7620cc4ac575b89d6e88bea7
Ritesh  Tambe <riteshtambe015@gmail.com>
2025-01-17 22:19:10 +0530

    Complete Project of Car Counter

--- midcomvis/Project 1 - Car Counter/Car-Counter.py
+++ midcomvis/Project 1 - Car Counter/Car-Counter.py
@@ -3,13 +3,18 @@ import cv2
 import cvzone
 import math
 import torch 
-
+from sort import * # Tracker Import 
 # web_url = "https://192.0.0.4:8000"
 
-cap = cv2.VideoCapture("../yolobasics/images/v5.mp4")
+cap = cv2.VideoCapture("../yolobasics/images/v4.mp4")
 
-maskImg = cv2.imread("mask.png")
+maskImg = cv2.imread("mask4.png")
 mask = cv2.resize(maskImg,(800,470))
+
+#Tracking Code  - Object Counter Companion code
+tracker = Sort(max_age = 20, min_hits=2,iou_threshold=0.3) 
+limits = [310,297,673,297] #cv2.line limit 
+totalCounts = []
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", 
               "truck", "boat", "traffic light", "fire hydrant", "stop sign", 
@@ -31,9 +36,20 @@ model.to(device)
 
 while True:
     success , img = cap.read()
+    if not success:
+        break
     img = cv2.resize(img,(800,470))
     imgRegion = cv2.bitwise_and(img,mask)
-    results = model(img,stream=True)
+    
+    
+
+    imgGraphics = cv2.imread("graphics.jpg",cv2.IMREAD_UNCHANGED)
+
+    if imgGraphics.shape[2]==4:
+        img = cvzone.overlayPNG(img,imgGraphics,(0,0))
+    results = model(imgRegion,stream=True)
+
+    detections = np.empty((0,5)) #Tracking Code  - Object Counter Companion code
 
     for r in results:
         boxes = r.boxes
@@ -52,9 +68,38 @@ while True:
             #Class Name
             cls = int(box.cls[0])
             currentClass = classNames[cls]
-            if currentClass =="car" or currentClass == "truck" and conf>0.9:
-                cvzone.putTextRect(img,f"{classNames[cls]} {conf}",(max(0,x1),max(35,y1)),scale = 1,thickness=1,offset=3)
-                cvzone.cornerRect(img,(x1,y1,w,h),l=9)
+            if currentClass =="car" and conf>0.6:
+                cvzone.putTextRect(img,f"{classNames[cls]} {conf}",(max(0,x1),max(35,y1)),
+                scale = 1,thickness=1,offset=3)
+                # cvzone.cornerRect(img,(x1,y1,w,h),l=9,rt=5)
+
+                #Tracking Code  - Object Counter Companion code
+                currentArray = np.array([x1,y1,x2,y2,conf])
+                detections = np.vstack((detections,currentArray))
+    resultsTracker=tracker.update(detections)
+
+    cv2.line(img,(limits[0],limits[1]),(limits[2],limits[3]),(0,0,255),5)
+    for result in resultsTracker:
+        x1,y1,x2,y2,Id = map(int,result)
+        # print(Id)
+        print(result) # Objet Counter
+        w,h = x2-x1,y2-y1
+        cvzone.cornerRect(img,(x1,y1,w,h),l=9,rt=5,colorR=(255,0,0))
+        cvzone.putTextRect(img,f"ID {Id}",(max(0,x1),max(35,y1)),
+                scale = 1,thickness=1,offset=3)
+
+        cx,cy=x1+w//2,y1+h//2
+        cv2.circle(img,(cx,cy),5,(255,0,0),cv2.FILLED)
+
+        if limits[0]<cx<limits[2] and limits[1]-20 < cy < limits[1]+20:
+            if totalCounts.count(Id)==0:
+                 totalCounts.append(Id)
+                 cv2.line(img,(limits[0],limits[1]),(limits[2],limits[3]),(0,255,0),5)
+
+                 
+
+
+    cvzone.putTextRect(img,f"Count:{len(totalCounts)}",(50,50))
 
     cv2.imshow("Image",img)
     cv2.imshow("ImageRegion",imgRegion)
